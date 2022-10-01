7fcc3ea20c153e3f98266b44357f39a57c404e45
mrli0988282303 <mrli0988282303@tcglinux9.cs.nctu.edu.tw>
2022-10-01 11:37:56 +0800

    greedy ok

--- project1/threes.cpp
+++ project1/threes.cpp
@@ -61,7 +61,7 @@ int main(int argc, const char* argv[]) {
 		if (stats.is_finished()) stats.summary();
 	}
 
-	random_slider slide(slide_args);
+	greedy_slider slide(slide_args);
 	random_placer place(place_args);
 
 	while (!stats.is_finished()) {
